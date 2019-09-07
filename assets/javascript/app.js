1c1e380689084ad4c0d9308f468aa181f5f99028
Bishop Bader <bflatbader@users.noreply.github.com>
2019-09-07 18:49:37 -0400

    got basic trivia functionality working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,21 +3,47 @@ function randomNumber (min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+function ask (i) {
+    $('#question').text(questions[i].question);
+    $('#a').text(questions[i].options[0]);
+    $('#b').text(questions[i].options[1]);
+    $('#c').text(questions[i].options[2]);
+    $('#d').text(questions[i].options[3]);
+}
+
+function checkAnswer (i, choice) {
+    if (choice == questions[i].correct){
+        $('#question').text("Correct!");
+        $('#a').empty();
+        $('#b').empty();
+        $('#c').empty();
+        $('#d').html('<img src="../assets/images/heart.gif">');
+
+        i++;
+        ask(i);
+        return i
+    } else {
+        console.log("incorrect answer");
+    }
+}
+
 // Variables
-var q1 = {
-    question : "What is Taylor's middle name?",
-    options : ["Pat", "Alison", "Melissa", "Britney"],
-    correct : 2
-}
-var q2 = {
-    question : "What was the name of Taylor's 3rd album?",
-    options : ["Taylor Swift", "Speak Now", "Red", "reputation"],
-    correct : 3
-}
-var q3 = {
-    question : "Which of the following is the name of one of Taylor's cats?",
-    options : ["Patches", "Muffin", "Fluffy", "Meredith"],
-    correct : 4
+var questions = {
+    1 : {
+        question : "What is Taylor's middle name?",
+        options : ["Pat", "Alison", "Melissa", "Britney"],
+        correct : 'b'
+    },
+    2 : {
+        question : "What was the name of Taylor's 3rd album?",
+        options : ["Taylor Swift", "Speak Now", "Red", "reputation"],
+        correct : 'c'
+    },
+    3 : {
+        question : "Which of the following is the name of one of Taylor's cats?",
+        options : ["Patches", "Muffin", "Fluffy", "Meredith"],
+        correct : 'd'   
+    }
 }
 
 i = 1;
@@ -25,11 +51,20 @@ i = 1;
 // Document Ready
 $(document).ready(function () {
 
+    // On start button click, hide the button and then unhide the game container
     $('button').on("click", function() {
         // Hide the Start button
         $('#start').addClass("hidden");
         // Show the game container
         $('#game').removeClass("hidden");
+    });
+
+    ask(i);
+
+    // 
+    $('.answers').on("click", function() {
+        var choice = $(this).attr('id');
+        i = checkAnswer(i, choice);
     });
 
 });
